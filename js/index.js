af9e0ed7d79e205799c30321681d5c82d0df8b34
孙佳 <207105858@qq.com>
2019-04-18 13:02:40 +0800

    第八次提交

--- js/index.js
+++ js/index.js
@@ -17,5 +17,33 @@ window.onload = function(){
 	var oH2 = oSecond.getElementsByTagName('h2')[0];
 	var oA2 = oH2.getElementsByTagName('a')[0];
 	// oA2.style.color = getColor();
+	// 
+	
+	//下落
+	function ball(op){
+		this._init(op);
+	}
+
+	ball.prototype = {
+		_init: function(option){
+			this.width = option.width;
+			this.height = option.height;
+			this.backgroundColor = option.backgroundColor;
+			this.speed = option.speed;
+
+		}
+		fly: function(){
+			
+		}
+	};
+
+	var ball = new ball({
+		width: 10,
+		height: 10,
+		backgroundColor: 'red',
+		speed: 2
+	});
+
+	console.log(ball);
 
 }
